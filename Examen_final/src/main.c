a43b8d18c7124f1aa8691439b7d040ba33b6b90d
Martín Pérez <martin.perez@eia.edu.co>
2026-06-01 11:03:23 -0500

    ultimo commit

--- Examen_final/src/main.c
+++ Examen_final/src/main.c
@@ -1,4 +1,25 @@
 uart_driver_init()
 adc_driver_init()
+timer_init()
 
-void app_main() {}
+float sensor_read(){
+    if (V>1.4){
+        N=95
+    }
+    else if (V<0.9){
+        N=42
+    }
+
+    float whipper_set_value(N)
+
+    return whipper_set_value
+}
+
+void app_main() {
+while(1){
+    if(timer_tiempo_ha_pasado(last_time, TIMER_PERIOD){
+        geofono = sensor_read();
+        uart_driver_send(geofono);
+    }
+}
+}
